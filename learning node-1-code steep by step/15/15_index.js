79d3d4688d66d73d4eb9612f5ff78009eb660191
vivek <vivekvekariya1999@gmail.com>
2023-03-18 09:57:44 -0700

    18-data get from param

--- learning node-1-code steep by step/15/15_index.js
+++ learning node-1-code steep by step/15/15_index.js
@@ -2,7 +2,9 @@ const express=require('express')
 const app=express()
 
 
-app.get("",(req,res)=>{
+app.get("/",(req,res)=>{
+    console.log("data from browser param ==>",req.query.name);   //browser url ma name="annundn" name ahiya console ma malse
+                                                                 //url ma localhost:4000/?name=anyyayy   refresh apata ahiya dat get thase
     res.send("this is nn home11 page");
 })
 app.get("/about",(req,res)=>{
