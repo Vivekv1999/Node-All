75d6742a9f5037bb9f0ff6d87467c3bb0c791747
vivek <vivekvekariya1999@gmail.com>
2023-03-18 10:45:06 -0700

    21-render html page

--- learning node-1-code steep by step/15/15_index.js
+++ learning node-1-code steep by step/15/15_index.js
@@ -1,27 +1,13 @@
 const express = require('express')
+const path=require('path')
+
 const app = express()
+const publicPath=path.join(__dirname,"public")
+
+app.use(express.static(publicPath))
 
 
-app.get("/", (req, res) => {
-    console.log("data from browser param ==>", req.query.name);   //browser url ma name="annundn" name ahiya console ma malse
-    //url ma localhost:4000/?name=anyyayy   refresh apata ahiya dat get thase
-    res.send(`<h1>home page</h1>
-    <a href="/about" >goto about page</a`);
-})
+console.log(publicPath);
 
-app.get("/about", (req, res) => {
-    res.send(`
-    <input type="text" placeholder="enter text" value="${req.query.name}"/>
-    <button>Click me</button>
-    <a href="/" >goto home page</a>
-    `);
-})
-app.get("/help", (req, res) => {
-    res.send([
-        { name: 'nil', email: 'nil@gmail.com' },
-        { name: 'Air', email: 'Air@gmail.com' }
-    ]
-    );
-})
 
 app.listen(4000)
