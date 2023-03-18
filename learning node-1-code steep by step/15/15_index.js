c23409eb1b5486bf063fddb9ce9315383c77a19f
vivek <vivekvekariya1999@gmail.com>
2023-03-18 10:14:28 -0700

    20-html json render in node

--- learning node-1-code steep by step/15/15_index.js
+++ learning node-1-code steep by step/15/15_index.js
@@ -1,17 +1,27 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 
 
-app.get("/",(req,res)=>{
-    console.log("data from browser param ==>",req.query.name);   //browser url ma name="annundn" name ahiya console ma malse
-                                                                 //url ma localhost:4000/?name=anyyayy   refresh apata ahiya dat get thase
-    res.send("this is nn home11 page");
+app.get("/", (req, res) => {
+    console.log("data from browser param ==>", req.query.name);   //browser url ma name="annundn" name ahiya console ma malse
+    //url ma localhost:4000/?name=anyyayy   refresh apata ahiya dat get thase
+    res.send(`<h1>home page</h1>
+    <a href="/about" >goto about page</a`);
 })
-app.get("/about",(req,res)=>{
-    res.send("this is about page");
+
+app.get("/about", (req, res) => {
+    res.send(`
+    <input type="text" placeholder="enter text" value="${req.query.name}"/>
+    <button>Click me</button>
+    <a href="/" >goto home page</a>
+    `);
 })
-app.get("/help",(req,res)=>{
-    res.send("this is HELP page");
+app.get("/help", (req, res) => {
+    res.send([
+        { name: 'nil', email: 'nil@gmail.com' },
+        { name: 'Air', email: 'Air@gmail.com' }
+    ]
+    );
 })
 
 app.listen(4000)
