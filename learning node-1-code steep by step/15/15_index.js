d1b3d3dbfec5d774f2d696c456c5ceae8a89a885
vivek <vivekvekariya1999@gmail.com>
2023-03-19 09:05:56 +0530

    22 remove extention from url and 404 page

--- learning node-1-code steep by step/15/15_index.js
+++ learning node-1-code steep by step/15/15_index.js
@@ -4,8 +4,18 @@ const path=require('path')
 const app = express()
 const publicPath=path.join(__dirname,"public")
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
 
+app.get("",(req,res)=>{
+res.sendFile(`${publicPath}/index.html`)
+})
+app.get("/about",(_,res)=>{
+res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get("*",(_,res)=>{
+    res.sendFile(`${publicPath}/PageNotFound.html`)
+})
 
 console.log(publicPath);
 
