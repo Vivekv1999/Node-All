c49982946236eae583899c7fc8489bb789284289
vivek <vivekvekariya1999@gmail.com>
2023-03-19 18:50:42 +0530

    #32 sameee

--- learning node-1-code steep by step/30 -mongo connect/index.js
+++ learning node-1-code steep by step/30 -mongo connect/index.js
@@ -1,17 +1,11 @@
 const dbconnect=require('./Mongodb')
 
-//-->1st method
-// dbconnect().then((res)=>{
-//     res.find().toArray().then((data)=>{
-//         console.log(data);
-//     })
-// })
-
-//--->second way to handle promise
+//-->read data--from mongo
 const main = async () => {
     let data = await dbconnect()
     data = await data.find().toArray()
     console.log(data);
 }
+
 
 main()
