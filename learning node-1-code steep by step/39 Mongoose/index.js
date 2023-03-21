793cb3f300260bd5fc076a2aaee8ebbe66135163
vivek <vivekvekariya1999@gmail.com>
2023-03-21 22:16:01 +0530

    #40 crude with mongoose

--- learning node-1-code steep by step/39 Mongoose/index.js
+++ learning node-1-code steep by step/39 Mongoose/index.js
@@ -1,20 +1,65 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
 
-const main=async ()=>{
-    await mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")  ////**imporant joylev */
-    const productSchema=new mongoose.Schema({
-        name:String,
-        price:Number
+// const main=async ()=>{
+//     await mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")  ////**imporant joylev */
+// } 
+
+// const productModal=mongoose.model('products',productSchema)
+// let data= new productModal({name:'asus zen',price:5050})
+// let result=await data.save()
+// console.log(result);
+
+// main ()
+
+
+mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")  ////**imporant joylev */
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    beand: String,
+    category: String
+})
+
+const savedataDb = async () => {
+    const product = mongoose.model('products', productSchema)
+    let data = new product({
+        name: 'Lg',
+        price: 5252,
+        category: 'mobile'
     })
-    
-
-    const productModal=mongoose.model('products',productSchema)
-    let data= new productModal({name:'asus zen',price:5050})
-    let result=await data.save()
+    let result = await data.save()
     console.log(result);
+}
 
-} 
+const updateDb = async () => {
+    const product = mongoose.model('products', productSchema)
+    let data = await product.updateOne(
+        { name: "note 11" },
+        {
+            $set: {
+                name: "realme 7",
+                price: 4555,
+                category: 'mobile'
+            }
+        }
+    )
+}
 
+const deleteDb = async () => {
+    const product = mongoose.model('products', productSchema)
+    const data= await product.deleteOne({name:"asus zen"})
+    console.log(data);
 
-main ()
+}
+const findDb = async () => {
+    const product = mongoose.model('products', productSchema)
+    const data= await product.find({name:"asus zen"})    //->badha jetla hase badha serch ma dekhase
+    console.log(data);
+}
+
+// savedataDb()
+// updateDb()
+// deleteDb()
+findDb()
