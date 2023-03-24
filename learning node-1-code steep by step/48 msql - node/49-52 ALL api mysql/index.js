53481098358b8a705ea71e4861ba41f655592dbc
vivek <vivekvekariya1999@gmail.com>
2023-03-24 08:36:59 +0530

    #50  mysql - post method

--- learning node-1-code steep by step/48 msql - node/49-52 ALL api mysql/index.js
+++ learning node-1-code steep by step/48 msql - node/49-52 ALL api mysql/index.js
@@ -1,18 +1,46 @@
-const express=require('express')
-const con=require('./config')
+const express = require('express')
+const con = require('./config')
 
-const app=express()
+const app = express()
 
 
-app.get("/",(req,res)=>{
-    con.query("select * from student",(err,result)=>{
-        if(err){
+app.get("/", (req, res) => {
+    con.query("select * from student", (err, result) => {
+        if (err) {
             res.send(err)
         }
-        else{
+        else {
             res.send(result)
         }
-    }) 
+    })
+})
+
+//static data add-post 
+// app.post('/', (req, res) => {
+//     const data = { studentname: "yaan", age: '32',nn:'oo'}     //table ma je field nu name hoy tej lakavu -- else request send thya j karse ostman ma error pn nay ave
+//     con.query('INSERT INTO student SET ?', data, (error, result, fields) => {
+//         if (error) {
+//            console.log(error)
+//         } else {
+//             res.send(result)
+//         }
+
+//     })
+// })
+
+app.use(express.json())
+
+//data add from postman
+app.post('/', async (req, res) => {
+    const data= await req.body
+    con.query('INSERT INTO student SET ?', data, (error, result, fields) => {
+        if (error) {
+           console.log(error)
+        } else {
+            res.send(result)
+        }
+
+    })
 })
 
 app.listen(4000)
